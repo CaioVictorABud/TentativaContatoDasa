90f0904fa374ac61d4dce4efd8a59d4c814cca6b
CaioVictorABud <153628584+CaioVictorABud@users.noreply.github.com>
2023-12-13 19:33:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const chamadoForm = document.getElementById("chamadoForm");
+    const mensagensContainer = document.getElementById("mensagensContainer");
+    const chamadosList = document.getElementById("chamadosList");
+
+    chamadoForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        adicionarChamado();
+    });
+});
+
+function gerarMensagem() {
+    const nome = document.getElementById("nome").value;
+    const numeroChamado = document.getElementById("numeroChamado").value;
+    const requisicao = document.getElementById("requisicao").value;
+
+    const mensagem = `Bom dia, aqui é do Suporte Dasa, é sobre o seu chamado ${numeroChamado}.\nRequisição: ${requisicao}\nEstá disponível para o atendimento?`;
+
+    const mensagemElement = document.createElement("div");
+    mensagemElement.classList.add("mensagem");
+    mensagemElement.innerText = mensagem;
+
+    const mensagensContainer = document.getElementById("mensagensContainer");
+    mensagensContainer.innerHTML = "";
+    mensagensContainer.appendChild(mensagemElement);
+
+    // Adicione lógica aqui para copiar a mensagem para a área de transferência se necessário
+}
+
+function adicionarChamado() {
+    const nome = document.getElementById("nome").value;
+    const numeroChamado = document.getElementById("numeroChamado").value;
+    const requisicao = document.getElementById("requisicao").value;
+
+    const chamado = { nome, numeroChamado, requisicao, status:
